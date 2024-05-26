f19f920c0cb3c0184cee5dbd6234efa6667a3bb7
Your Name <igurtest@gmail.comm>
2024-05-26 22:47:37 +0530

    dfds

---
+++ pageObjects/BusiApplePage.py
@@ -0,0 +1,122 @@
+from selenium.webdriver.common.by import By
+import json
+
+class BusiApplePage:
+    textbox_username_xpath = "/html/body/app-root/app-login/div/div/div[1]/div/div/div/div[3]/div/input"
+    button_showpayments_xpath = "/html/body/app-root/app-layout/div/section/div/app-details/div[3]/div[2]/div/button"
+
+    text_financed_amount_xpath = "/html/body/app-root/app-layouts/mat-sidenav-container/mat-sidenav-content/main/app-contract-list/div[1]/div[2]/div[2]/app-contract-details/div/div/div[2]/mat-accordion[1]/mat-expansion-panel/div/div/div/div/div/div[1]/div[1]/div/label[2]/span"
+    text_downpayment_amount_xpath = "/html/body/app-root/app-layouts/mat-sidenav-container/mat-sidenav-content/main/app-contract-list/div[1]/div[2]/div[2]/app-contract-details/div/div/div[2]/mat-accordion[1]/mat-expansion-panel/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/h4"
+    text_no_of_month_xpath = ""
+    
+    # value_no_of_month_xpath = "/html/body/app-root/app-layout/div/section/div/app-details/div[3]/div[1]/div[1]/div[2]/div/div[2]/div[1]/div/div[6]/h6"
+    id_panel_financed_amount="financed_amount"
+    id_panel_down_payment="downpayment_amount"
+    value_no_of_month_id = "number_of_payments"
+    value_intrest_id = "interest_rate"
+    id_panelrecurring = "recurring_amount"
+    id_panelupfrontfee = "upfront_fee"
+    id_panelExpectedmonthlypayouttobusiness = "expected_monthly_pay_to_doctor"
+    id_panelTotalExpectedPayouttoBusiness = "doctor_expected_payout"
+    id_panelTotalRemainingAmount = "remaining_amount"
+    id_panelCustomerPayoffAmount = "patient_payoff_amount"
+    id_panelBusinessExpectedPayoff = "doctor_expected_payoff"
+    id_panelPrincipalAmount = "recurring_without_interest"
+    id_panelTotalBalanceRemaining = "total_balance_remaining"
+    
+    value_intrest_xpath = "after_deferred_interest_rate"
+    
+    # textbox_password_xpath = "/html/body/app-root/div/div/app-login/div/form/div[1]/div[2]/input"
+    # textbox_password_xpath = "/html/body/app-root/app-login/div[2]/div/form/div[1]/div/input"
+
+    # button_login_xpath = "/html/body/app-root/app-login/div[2]/div/form/div[2]/button[1]"
+
+    # button_logout_drop_xpath = "/html/body/app-root/div/div/app-dashboard/div/div[2]/div[1]/div/div[2]/div/app-profile-drop-down/header/div/div/button"
+    # button_logout_xpath = "/html/body/app-root/div/div/app-dashboard/div/div[2]/div[1]/div/div[2]/div/app-profile-drop-down/header/div/div/ul/li[7]/a"   
+    # button_logout_last_xpath = "/html/body/modal-container/div[2]/div/app-logout-confirm/div[2]/div/button[2]"
+ 
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    # def setUserName(self, username):
+    #     self.driver.find_element(By.XPATH, self.textbox_username_xpath).clear()
+    #     self.driver.find_element(By.XPATH, self.textbox_username_xpath).send_keys(username)
+
+    # def setPassword(self, password):
+    #     self.driver.find_element(By.XPATH, self.textbox_password_xpath).clear()
+    #     self.driver.find_element(By.XPATH, self.textbox_password_xpath).send_keys(password)
+    # def get_all_values(self ):
+        
+        # button_showpayments_xpath = "/html/body/app-root/app-layout/div/section/div/app-details/div[3]/div[2]/div/button"
+
+        # Text_finced_amount_xpath = "/html/body/app-root/app-layout/div/section/div/app-details/div[3]/div[1]/div[1]/div[2]/div/div[2]/div[1]/div/div[2]/h6"
+        # value_downp_amount_xpath = "/html/body/app-root/app-layout/div/section/div/app-details/div[3]/div[1]/div[1]/div[2]/div/div[2]/div[1]/div/div[3]/h6"
+        # # value_no_of_month_xpath = "/html/body/app-root/app-layout/div/section/div/app-details/div[3]/div[1]/div[1]/div[2]/div/div[2]/div[1]/div/div[6]/h6"
+        # value_no_of_month_id = "number_of_payments"
+        # value_intrest_id = "interest_rate"
+        # id_panelrecurring = "recurring_amount"
+        # id_panelupfrontfee = "upfront_fee"
+        # id_panelExpectedmonthlypayouttobusiness = "expected_monthly_pay_to_doctor"
+        # id_panelTotalExpectedPayouttoBusiness = "doctor_expected_payout"
+        # id_panelTotalRemainingAmount = "remaining_amount"
+        # id_panelCustomerPayoffAmount = "patient_payoff_amount"
+        # id_panelBusinessExpectedPayoff = "doctor_expected_payoff"
+        # id_panelPrincipalAmount = "recurring_without_interest"
+        # id_panelTotalBalanceRemaining = "total_balance_remaining"
+
+    # def clickShowPayments(self):
+    #     self.driver.find_element(By.XPATH, self.button_showpayments_xpath).click()
+
+    def textfinacedamount(self):
+        self.value_finaced_ammount=(self.driver.find_element(By.XPATH, self.text_financed_amount_xpath).text)
+        print(self.value_finaced_ammount)
+    def textdownpaymentamount(self):
+
+        self.text_downpayment_amount=(self.driver.find_element(By.ID, self.text_downpayment_amount_xpath).text)
+        print(self.text_downpayment_amount)    
+    
+    def textnumberofmonth(self):
+        self.text_number_of_month=(self.driver.find_element(By.ID, self.text_no_of_month_xpath).text)
+        print("errrorhare")
+        print(self.text_number_of_month)    
+    # def valueoftheintrest(self):
+    #     self.value_of_intrest=(self.driver.find_element(By.ID, self.value_intrest_id).text)
+    #     print(self.value_of_intrest)
+
+    # def clickLogoutDrop(self):
+    #     self.driver.find_element(By.XPATH, self.button_logout_drop_xpath).click()
+
+    # def clickLogout(self):
+    #     self.driver.find_element(By.XPATH, self.button_logout_xpath).click()
+
+    # def clickLogoutLast(self):
+    #     self.driver.find_element(By.XPATH, self.button_logout_last_xpath).click()
+    # def panelrecurring(self):
+    #     self.value_panelrecurring=(self.driver.find_element(By.ID, self.id_panelrecurring).text)
+    #     print(self.value_panelrecurring)
+    # def panelupfrontfee(self):
+    #     self.value_panelupfrontfee=(self.driver.find_element(By.ID, self.id_panelupfrontfee).text)
+    #     print(self.value_panelupfrontfee)
+    # def panelExpectedmonthlypayouttobusiness(self):
+    #     self.value_panelExpectedmonthlypayouttobusiness=(self.driver.find_element(By.ID, self.id_panelExpectedmonthlypayouttobusiness).text)
+    #     print(self.value_panelExpectedmonthlypayouttobusiness)
+    # def panelTotalExpectedPayouttoBusiness(self):
+    #     self.value_panelTotalExpectedPayouttoBusiness=(self.driver.find_element(By.ID, self.id_panelTotalExpectedPayouttoBusiness).text)
+    #     print(self.value_panelTotalExpectedPayouttoBusiness)
+    # def panelTotalRemainingAmount(self):
+    #     self.value_panelTotalRemainingAmount=(self.driver.find_element(By.ID, self.id_panelTotalRemainingAmount).text)
+    #     print(self.value_panelTotalRemainingAmount)
+    # def panelCustomerPayoffAmount(self):
+    #     self.value_panelCustomerPayoffAmount=(self.driver.find_element(By.ID, self.id_panelCustomerPayoffAmount).text)
+    #     print(self.value_panelCustomerPayoffAmount)
+    # def panelBusinessExpectedPayoff(self):
+    #     self.value_panelBusinessExpectedPayoff=(self.driver.find_element(By.ID, self.id_panelBusinessExpectedPayoff).text)
+    #     print(self.value_panelBusinessExpectedPayoff)
+    # def panelPrincipalAmount(self):
+    #     self.value_panelPrincipalAmount=(self.driver.find_element(By.ID, self.id_panelPrincipalAmount).text)
+    #     print(self.value_panelPrincipalAmount)
+    # def panelTotalBalanceRemaining(self):
+    #     self.value_panelTotalBalanceRemaining=(self.driver.find_element(By.ID, self.id_panelTotalBalanceRemaining).text)  
+    #     print(self.value_panelTotalBalanceRemaining)
+    #     return {'value_finaced_ammount': value_finaced_ammount , 'value_of_downpayment':value_of_downpayment , 'value_of_no_of_month': value_of_no_of_month , 'value_of_intrest' : value_of_intrest} 
---
+++ testCases/test_busiapple.py
@@ -0,0 +1,76 @@
+import pytest
+from pageObjects.LoginPageBusi import LoginPageBusi
+from pageObjects.AdminCDPage import AdminCDPage
+from testCases.Busi_get_all import  Busi_get_all
+from testCases.calculate import calculate
+from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
+import time
+
+
+class Test_001_Login:
+    # baseURL = "https://testcustomer.denefits.com"
+    baseURL = "https://testbusiness.denefits.com"
+    username = "gurdeep.singh+blacklabel@bridgingtech.com"
+    password = "sookie"
+    logger = LogGen.loggen()
+    def getAllfunc():
+        printO("called")
+
+
+
+    def test_login(self, setup ):
+
+        self.logger.info("****Started Login Test****")
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        self.lp = LoginPageBusi(self.driver)
+        self.lp.setUserName(self.username)
+        time.sleep(7)
+        self.lp.clickContinue()
+        self.lp.setPassword(self.password)
+        time.sleep(5)
+        self.lp.clickLogin()
+        time.sleep(25)
+        act_title = self.driver.title
+        time.sleep(5)
+        print(act_title)
+
+
+        if act_title == "Denefits Business":
+            self.logger.info("****Login test passed ****")
+            print("2t")
+            
+            self.lp.clickPaymentPlan()
+            time.sleep(5)
+            self.lp.clickManageContract()
+            time.sleep(25)
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_where_i_m.png")
+            self.lp.clickFullPaymentPlanDetails()
+            time.sleep(5)
+            self.get=Busi_get_all(self.driver)
+            time.sleep(5)
+            panelresult = self.get.getAll()
+
+
+
+            
+
+ 
+ 
+
+        else:
+            self.logger.error("****Login test failed ****")
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_busiapple.png")
+            self.driver.close()
+            print("2ffffffffffffffffffffffffffffff")
+
+
+
+   
+         
+        # def fun_caluculate(self):
+        #     cd = AdminCDPage(self.driver)
+        #     cd.textfincedamount()
+        #     finaced_amount = self.cd.s
+        #     print(finaced_amount) 

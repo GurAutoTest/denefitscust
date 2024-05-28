3b09c6e120f254524fef039bc0a0c274c477b1f8
GurAutoTest <igurtest@gmail.com>
2024-05-27 20:38:22 -0700

    testtttttttttttttttttttttttttt

--- run.bat
+++ run.bat
@@ -6,4 +6,16 @@ pytest -v -s testCases/test_login_ddt.py
 
 pytest -v -s testCases/test_admin.py 
 
+
+pytest -v -s testCases/test_editcontractdef.py 
+
 pytest -v -s testCases/test_linkedin.py 
+
+set env variable
+
+
+
+pip install pytest
+pip install selenium
+geckodriver_autoinstaller
+openpyxl

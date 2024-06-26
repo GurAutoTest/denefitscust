911073abb7e8e672ecea7ebeac8d1aaeefc3c8f7
GurAutoTest <igurtest@gmail.com>
2024-06-26 16:01:21 +0530

    xxx

--- testCases/busicalculate.py
+++ testCases/busicalculate.py
@@ -35,9 +35,11 @@ class busicalculate:
             # print(d)
             # print("abc")
             # print(self.finaced_amount)
-            # finaced_amount = a.replace("$","").replace(",","")
+            finaced_amount = a
+            # .replace("$","").replace(",","")
             # print(finaced_amount)
-            # down_payment = b.replace("$","").replace(",","")
+            down_payment = b
+            # .replace("$","").replace(",","")
             # print(down_payment)
             no_of_month = int(c)
             print(no_of_month)
@@ -51,22 +53,32 @@ class busicalculate:
 
             # print(type(g))
             if (interest == 19.9):
-             totel_payble_with_intrest_flat = (a*1.1990)
+             totel_payble_with_intrest_flat = (finaced_amount*1.1990)
+             totel_payble_with_intrest_flat= round(totel_payble_with_intrest_flat)
              print("flat",totel_payble_with_intrest_flat)
              # print("totel payble   =  "  +str(c2)) 
-             principle_per_month=a/ no_of_month
+             principle_per_month=finaced_amount/ no_of_month
+             principle_per_month=round(principle_per_month)
              recuring_amont_during_def_with_transection  =  principle_per_month * 1.04
+             recuring_amont_during_def_with_transection =round(recuring_amont_during_def_with_transection)
              # print("Recuring amount during deffred     =  "  + str(recuring_amont_during_def_with_transection))  
              totel_remaning_during_def= recuring_amont_during_def_with_transection*no_of_month
+             totel_remaning_during_def= round(totel_remaning_during_def)
               # print("totel_reaning_during def  =  "   + str(totel_remaning_during_def))   
             
              new_uf =  totel_payble_with_intrest_flat / no_of_month + 1
              rec_after_def =  totel_payble_with_intrest_flat / no_of_month + 1
+             rec_after_def= round(rec_after_def)
 
              customer_payoff_amount = recuring_amont_during_def_with_transection * no_of_month
+             customer_payoff_amount = round(customer_payoff_amount)
           
              expected_monthly_payout = principle_per_month*0.995
+             expected_monthly_payout = round(expected_monthly_payout)
+
+             
              remaining_expected_payout = expected_monthly_payout * no_of_month
+             remaining_expected_payout = round(remaining_expected_payout)
              Paid_to_Business = "working on it"
              donatedamount = "working on it"
 
@@ -135,17 +147,25 @@ class busicalculate:
               # print("compound",totel_payble_with_intrest_compund)
               
               
-
-
-
-
-
-
-      def compare(self , a,b):
+      def compare(self, a, b):
          print("inside compare")
 
          print(a)
          print(b)
+         dict1 = json.loads(a)
+         dict2 = json.loads(b)
+         for key in dict1:
+                if key in dict2:
+                   if dict1[key] != dict2[key]:
+                     print(f"Difference found at key '{key}': '{dict1[key]}' vs '{dict2[key]}'")
+                else:
+                    print(f"Key '{key}' not found in second JSON")
+    
+    # Check for keys in dict2 not in dict1
+         for key in dict2:
+                if key not in dict1:
+                  print(f"Key '{key}' not found in first JSON")
+
 
 
             
--- testCases/tempCodeRunnerFile.py
+++ testCases/tempCodeRunnerFile.py
@@ -1,4 +1,24 @@
-a  = 1247.58
-ra=round(a)
-print(ra)
-print(round(1247.58))
+import json
+
+def compare_jsons(json1, json2):
+    dict1 = json.loads(json1)
+    dict2 = json.loads(json2)
+    
+    for key in dict1:
+        if key in dict2:
+            if dict1[key] != dict2[key]:
+                print(f"Difference found at key '{key}': '{dict1[key]}' vs '{dict2[key]}'")
+        else:
+            print(f"Key '{key}' not found in second JSON")
+    
+    # Check for keys in dict2 not in dict1
+    for key in dict2:
+        if key not in dict1:
+            print(f"Key '{key}' not found in first JSON")
+
+# Example JSON strings
+json1 = '{"name": "John", "age": 30, "city": "New York"}'
+json2 = '{"name": "John", "age": 30, "city": "Los Angeles"}'
+
+# Call the function to compare
+compare_jsons(json1, json2)
